5b932f7667465cbba653294f29c28fa10356180d
Ajay-debug91 <68456769+Ajay-debug91@users.noreply.github.com>
2021-01-26 15:06:19 +0530

    new changes

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -2,14 +2,20 @@ package com.ashokit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+
+import com.ashokit.beans.Car;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.ashok","com.ashokit"})// it's overriding default ComponentScaning 
 public class Application {
-
+//change priority task 12pm no 102
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		Car car = context.getBean(Car.class);
+		car.getClass();
+		System.out.println(car.getClass());
 	}
 	
 	public void displayMessage()
